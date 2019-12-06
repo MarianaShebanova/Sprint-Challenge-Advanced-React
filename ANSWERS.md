a78cacef2868ede1c6b2279cbe7357ee77a58aef
MarianaShebanova <mariana.shebanova@gmail.com>
2019-12-06 10:52:43 -0800

    Sprint-Challenge-Advanced-React

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,4 +1,22 @@
 - [ ] Why would you use class component over function components (removing hooks from the question)?
+
+if you need lifecycle hooks you should probably use a class component.
+If you need a state in your component you will either need to create a class component or you lift the state up to the parent component and pass it down the functional component via props
+
 - [ ] Name three lifecycle methods and their purposes.
+
+mounting, updating, unmounting
+
 - [ ] What is the purpose of a custom hook?
+
+Custom Hooks are a mechanism to reuse stateful logic (such as setting up a subscription and remembering the current value), but every time you use a custom Hook, all state and effects inside of it are fully isolated.
+
 - [ ] Why is it important to test our apps?
+
+Surfaces bugs faster.
+Reduces the risk of regressions.
+Allows us to trust the code.
+Makes us think about the edge cases.
+Acts as a safety net when making changes or refactoring.
+Acts as documentation for the code.
+Encourages us to write more testable (better!) code.
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './App.css';
+import Players from './components/Players';
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { players: [] };
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/api/players`).then(response => {
+      this.setState({ players: response.data });
+    })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <Navbar />
+        </div>
+        <Players players={this.state.players} />
+      </div>
+    );
+  }
 }
 
 export default App;
